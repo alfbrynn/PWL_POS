3524c982e75bf8e6700f146bf0ba4793f75062cf
alfbrynn <aliffebriansyah@gmail.com>
2025-03-10 04:26:46 +0700

    isDirty, isClean, wasChanged, debugging

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -42,15 +42,33 @@ class UserController extends Controller
         // dd($user);
 
         // Praktikum 2.4
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager33',
+        //         'nama' => 'Manager Tiga Tiga',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ],
+        // );
+        // $user->save();
+        // return view('user', ['data' => $user]);
+
+        // Praktikum 2.5
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+
+        $user->username = 'manager12';
+
         $user->save();
-        return view('user', ['data' => $user]);
+
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
+        dd($user->wasChanged('nama', 'username')); // true
     }
 }
