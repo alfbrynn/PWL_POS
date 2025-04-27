a71eca4ac2b468b0e71caae0a7a5c733b225d0d2
alfbrynn <aliffebriansyah@gmail.com>
2025-04-27 20:08:29 +0700

    level api

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Api\LevelController;
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
@@ -20,3 +10,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
+// Route::group(['prefix' => 'levels'], function () {
+//     Route::get('levels', [LevelController::class, 'index']);
+//     Route::post('levels', [LevelController::class, 'store']);
+//     Route::get('levels/{level}', [LevelController::class, 'show']);
+//     Route::put('levels/{level}', [LevelController::class, 'update']);
+//     Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+// });
+
+Route::apiResource('levels', LevelController::class);
